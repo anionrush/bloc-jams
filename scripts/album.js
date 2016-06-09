8782c46d4169c712ff45072cc640c5cca217bbd0
Onur Sahin <anionrush@gmail.com>
2016-06-08 18:05:22 -0700

    Assignment 27 completed

--- scripts/album.js
+++ scripts/album.js
@@ -77,7 +77,16 @@ var currentlyPlayingSong = null;
         if (element) {
             var currentParent = element.parentElement;
             while (currentParent.className != targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
+                
+                    currentParent = currentParent.parentElement;
+                
+                    if(currentParent.classname === null){
+                        alert("No parent found");
+                    }    
+                    else if (currentParent.className != targetClass ){
+                        alert("No parent found with that class name");
+                    }
+
             }
         return currentParent;
         }
